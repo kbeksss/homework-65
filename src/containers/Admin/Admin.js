024464a00890840249d8777459a6a4865bd643ee
kbeksss <kbeksss@outlook.com>
2019-12-26 17:40:49 +0600

    Form that gets page names and changes values of inputs

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,39 +1,57 @@
 import React, {Component} from 'react';
 import {Button, Col, Form, FormGroup, Input} from "reactstrap";
+import axiosAPI from "../../axios-page";
 
 class Admin extends Component {
     state = {
         pageName: '',
-        title: '',
-        description: '',
+        title: 'aaa',
+        description: 'aa',
+        pages: [],
     };
-    changeValue = e => this.setState({[e.target.name]: e.target.value} );
+    changeValue = e => {
+        this.setState({[e.target.name]: e.target.value} );
+    };
     submitForm = e => {
         e.preventDefault();
-        console.log('submit')
+
+        console.log('submit');
     };
+    getData = async (name) => {
+        let response = await axiosAPI.get('pages/' + name + '.json');
+        this.setState({title: response.data.title, description: response.data.description});
+    };
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevState.pageName !== this.state.pageName){
+            return this.getData(this.state.pageName);
+        }
+    }
+    componentDidMount = async () => {
+        let response = await axiosAPI.get('pages.json');
+        console.log(response.data);
+        this.setState({pages: Object.keys(response.data)});
+    };
+
     render() {
-        console.log(this.state);
+        let pageNames = this.state.pages ? this.state.pages.map(pageName => <option key={pageName} value={pageName}>{pageName}</option>) : null;
         return (
             <div className='mt-5'>
                 <Form onSubmit={this.submitForm}>
                     <FormGroup>
                         <Col sm={5}>
-                            <Input type="text" name="title" id="title" placeholder="Title" onChange={this.changeValue}/>
+                            <Input type="text" value={this.state.title} name="title" id="title" placeholder="Title" onChange={this.changeValue}/>
                         </Col>
                     </FormGroup>
                     <FormGroup>
                         <Col sm={4}>
                             <Input type="select" name='pageName' onChange={this.changeValue}>
-                                <option>Home</option>
-                                <option>Contacts</option>
-                                <option>About</option>
+                                {pageNames}
                             </Input>
                         </Col>
                     </FormGroup>
                     <FormGroup>
                         <Col sm={7}>
-                            <Input type="textarea" name="description" id="text" onChange={this.changeValue}/>
+                            <Input type="textarea" value={this.state.description} name="description" id="text" onChange={this.changeValue}/>
                         </Col>
                     </FormGroup>
                     <Button>Save</Button>
